af99fb581b9ece1fed3f7fa54c6c396db6d39276
Ben_Puls <discordben7@gmail.com>
2024-03-25 07:14:55 +0800

    new version

--- GUI/src/main.py
+++ GUI/src/main.py
@@ -1,7 +1,8 @@
 import customtkinter
 # import tkinter as tk
 from tkinter import filedialog
-
+from PIL import Image
+import webbrowser
 # from tkinter import messagebox
 
 # import temp.actions.load as load
@@ -28,20 +29,35 @@ def converting():
     print('Converting')
 
 
+def github_link():
+    webbrowser.open('https://github.com/byBenPuls/spectrumconverter')
+
+
 app = customtkinter.CTk()
 app.geometry('450x250')
 app.title('Spectrum Converter')
 app.iconbitmap('../resources/icons/icon.ico')
 app.resizable(False, False)
-
+app.grid_columnconfigure((0), weight=1)
+app.grid_rowconfigure((0), weight=1)
 button = customtkinter.CTkButton(app, text='Нажмите, чтобы выбрать файл', command=button_callback)
-button.pack(padx=20, pady=20)
+button.grid(column=0, row=0)
 button1 = customtkinter.CTkButton(app, text='конвертировать',
                                   border_width=1,
                                   state=customtkinter.DISABLED,
                                   fg_color='green',
                                   hover_color='#005B00',
-                                  command=converting)
-button1.pack(padx=20, pady=70)
+                                  command=converting,
+                                  height=40)
+button1.grid(column=0, row=1)
+
+github_image = customtkinter.CTkImage(Image.open('../resources/icons/github-mark-white.png'),
+                                      size=(30, 30))
+
+image_label = customtkinter.CTkButton(app, image=github_image, text='',
+                                      fg_color='transparent', hover=False,
+                                      command=github_link,
+                                      width=30)
+image_label.grid(column=3, row=3)
 
 app.mainloop()
